2569fd82e05c4374b9e633d263be681f579f548d
Abhinav Bhardwaj <abhinav.bhardwaj@engineer.com>
2017-11-17 06:56:09 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 @session_start();
 ini_set('display_errors', 1);
-define('clientId', '4cb5ca8e039945429574b81abe6c5e77');
-define('clientSecret', '2aae57d5217448ff8977c397b39731d6');
-define('redirectUri', 'http://localhost/abhinav/instagram/welcome.php');
-define('siteUri', 'http://localhost/abhinav/instagram/');
+define('clientId', '');
+define('clientSecret', '');
+define('redirectUri', 'http://localhost/instagram-php-sdk/welcome.php');
+define('siteUri', 'http://localhost/instagram-php-sdk/');
 
 function prd($string, $noDie =false){
 	echo '<pre>';
